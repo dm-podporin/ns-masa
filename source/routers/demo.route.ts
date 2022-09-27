c25ff9845c0a73290ecd98ffc1ae317fb24f5d14
deadhecore <dm.podporin@gmail.com>
2022-09-27 18:27:00 +0300

    timeout adding and testing

--- source/routers/demo.route.ts
+++ source/routers/demo.route.ts
@@ -4,5 +4,6 @@ import controller from '../controllers/demo.controler'
 const router = express.Router();
 
 router.get('/hello-world', controller.getHelloWorld)
+router.get('/timeout', controller.getWithTimeout)
 
 export = router;
