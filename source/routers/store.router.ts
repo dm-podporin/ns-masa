e59441204982f78fac37a1e39d026311a3fa731c
deadhecore <dm.podporin@gmail.com>
2022-10-26 15:40:46 +0300

    Refactor of Request parse of numeric input added

--- source/routers/store.router.ts
+++ source/routers/store.router.ts
@@ -6,5 +6,6 @@ const router = express.Router();
 router.get('/stores', controller.getStores)
 router.get('/storeById/:store_id', controller.getStoreById)
 router.get('/storeByCity/:city', controller.getStoreByCity);
+router.put('/updstoreById/:store_id', controller.updateStoreById);
 
 export = router;
